467daafb9e64d5932a70b47a34c440da9d9fba9c
lperepol <lawrence.perepolkin@gmail.com>
2022-07-26 22:24:16 +0000

    Post working

--- public_html/js/UNL/ImagesByNid.js
+++ public_html/js/UNL/ImagesByNid.js
@@ -4,39 +4,77 @@ var currentImageKey = null;
 var currentImageNum = null;
 var currentImage = null;
 
-$(document).ready(function() {
+var ImageName = null;
+var ImageText = null;
+var Gender = null;
+var General = null;
+var Descr = null;
+var Detail = null;
+var Magnification = null;
+var Location = null;
+var Host = null;
+var Species = null;
+var ScientificName_accepted = null;
+var ScientificName = null;
+var Class = null;
+var Order = null;
+var Family = null;
+var Genus = null;
+var Species = null;
+
+
+$(document).ready(function () {
     /*********************************************************************/
     $.ajaxSetup({
-        beforeSend: function() {
+        beforeSend: function () {
             // show gif here, eg:
             //$('#loading').show();
             $('body').addClass('loading');
         },
-        complete: function() {// hide gif here, eg:
+        complete: function () {// hide gif here, eg:
             $('body').removeClass('loading');
+            ImageTextSel = document.getElementById('ImageText');
+            ImageNameSel = document.getElementById('ImageName');
+            ImageTextSel = document.getElementById('ImageText');
+            GenderSel = document.getElementById('Gender');
+            GeneralSel = document.getElementById('General');
+            DescrSel = document.getElementById('Descr');
+            DetailSel = document.getElementById('Detail');
+            MagnificationSel = document.getElementById('Magnification');
+            LocationSel = document.getElementById('Location');
+            HostSel = document.getElementById('Host');
+            SpeciesSel = document.getElementById('Species');
+            ScientificName_acceptedSel = document.getElementById('ScientificName_accepted');
+            ScientificNameSel = document.getElementById('ScientificName');
+            ClassSel = document.getElementById('Class');
+            OrderSel = document.getElementById('Order');
+            FamilySel = document.getElementById('Family');
+            GenusSel = document.getElementById('Genus');
+            SpeciesSel = document.getElementById('Species');
             displayUNLImage();
         }
     });
     /*********************************************************************/
 
-        var arr = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': '/files/json/unl/ImagesByNid.json',
+    var arr = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': '/files/json/unl/ImagesByNid.json',
 
-            'dataType': 'json',
-            'success': function (data) {
-                imageCollection = data;
-                currentImageNum = 0
-                currentImageKey = Object.keys(imageCollection)[currentImageNum];
-                currentImage = currentImageKey;
-                console.log('Current imagekey:\n' + currentImageKey);
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                alert('Status: ' + textStatus); alert('Error: ' + errorThrown);
-            }
-        });
+        'dataType': 'json',
+        'success': function (data) {
+            imageCollection = data;
+            currentImageNum = 0
+            currentImageKey = Object.keys(imageCollection)[currentImageNum];
+            currentImage = currentImageKey;
+            console.log('Current imagekey:\n' + currentImageKey);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert('Status: ' + textStatus);
+            alert('Error: ' + errorThrown);
+        }
+    });
 
 });
 
@@ -54,12 +92,46 @@ function displayUNLImage() {
     $('#displayDiv').empty();
     $('#displayDiv').append(image);
 
+    var ImageText = imageCollection[currentImage][1];
+    ImageTextSel.value = ImageText;
+   
+   
+    var SpeciesText = imageCollection[currentImage][16];
+    SpeciesSel.value = SpeciesText;
+   
+    var GenderText = imageCollection[currentImage][2];
+    GenderSel.value = GenderText;
+
+    var DetailText = imageCollection[currentImage][5];
+    DetailSel.value = DetailText;
+
+    var MagnificationText = imageCollection[currentImage][6];
+    MagnificationSel.value = MagnificationText;
+    
+    var LocationText = imageCollection[currentImage][7];
+    LocationSel.value = LocationText;
+    
+    var HostText = imageCollection[currentImage][8];
+    HostSel.value = HostText;
+
+    /*
+    LocationSel.value = LocationText;
+    HostSel.value = HostText;
+    SpeciesSel.value = SpeciesText;
+    ScientificName_acceptedSel.value = ScientificName_acceptedText;
+    ScientificNameSel.value = ScientificNameText;
+    ClassSel.value = ClassText;
+    OrderSel.value = OrderText;
+    FamilySel.value = FamilyText;
+    console.log('Image data:\n' + ImageText);
+    */
+
     //currentImage = imageCollection[currentImageKey];
 
     return false;
 }
 function resetImages() {
-    count =1;
+    count = 1;
     location.reload();
 }
 function nextImage() {
@@ -75,10 +147,10 @@ function nextImage() {
 function previousImage() {
     currentImageNum = currentImageNum - 1;
     if (currentImageNum < 1) {
-       currentImageNum = 0; 
+        currentImageNum = 0;
     }
 
-    
+
     currentImageKey = Object.keys(imageCollection)[currentImageNum];
     currentImage = currentImageKey;
     console.log('imageCollection:\n' + imageCollection);
